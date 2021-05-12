4ba67a7cdc4157786fee254afb989c9f1ac19238
Abdur rahman <abdur.rahman200301@gmail.com>
2021-05-12 14:33:10 +0600

    Setup Next Js

---
+++ pages/about.js
@@ -0,0 +1,9 @@
+export default function AboutPage() {
+    return (
+        <div>
+            <h1>About Page</h1>
+            <p> This Is App To find TO Latest DJ And Other Musical Event </p>
+            <p>Version 1.0.0</p>
+        </div>
+    )
+}
