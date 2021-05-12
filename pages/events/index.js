dcd250c0dc4fc0243e183f19bf441b295041dbce
Abdur rahman <abdur.rahman200301@gmail.com>
2021-05-12 16:07:54 +0600

    Bascis Design done

--- pages/events/index.js
+++ pages/events/index.js
@@ -1,8 +1,8 @@
-import Layout from "../../components/Layout";
+import Layout from "@/components/Layout";
 
 export default function EventsPage() {
   return (
-    <Layout title="My Event">
+    <Layout title="DJ Events">
       <h1>My Event Page</h1>
     </Layout>
   );
