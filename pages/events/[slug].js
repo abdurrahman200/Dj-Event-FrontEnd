06d0d7af14528d6249485fef12e988812858fbe4
Abdur rahman <abdur.rahman200301@gmail.com>
2021-05-12 20:42:13 +0600

    frontend done

--- pages/events/[slug].js
+++ pages/events/[slug].js
@@ -1,9 +1,50 @@
+import Link from "next/link";
+import Image from "next/image";
 import Layout from "@/components/Layout";
+import { API_URL } from "@/config/index";
+import styles from "@/styles/Event.module.css";
 
-export default function EventPage() {
+export default function EventPage({ evt }) {
   return (
     <Layout title="Single Event | Slug">
-      <h1>Single Event Page</h1>
+      <div className={styles.event}>
+        <div className={styles.controls}>
+          <Link href={`/events/edit/${evt.id}`}> Edit </Link>
+          <span className={styles.delete}>
+            <Link href={`/events/edit/${evt.id}`}>
+              <a> Delete </a>
+            </Link>
+          </span>
+        </div>
+
+        <span>
+          {new Date(evt.date).toLocaleDateString("en-US")} at {evt.time}
+        </span>
+        <h1>{evt.name}</h1>
+
+        {evt.image && (
+          <div className={styles.image}>
+            <Image src={evt.image} width={960} height={600} />
+          </div>
+        )}
+
+        <h3>Performers:</h3>
+        <p>{evt.performers}</p>
+        <h3>Description:</h3>
+        <p>{evt.description}</p>
+        <h3>Venue: {evt.venue}</h3>
+        <p>{evt.address}</p>
+      </div>
     </Layout>
   );
+}
+
+export async function getServerSideProps({ query: { slug } }) {
+  const res = await fetch(`${API_URL}/api/events/${slug}`);
+  const events = await res.json();
+  return {
+    props: {
+      evt: events[0],
+    },
+  };
 }
