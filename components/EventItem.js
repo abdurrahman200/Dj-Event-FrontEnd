3c21261b4d49115d4baf230037717e690d6ffe9d
Abdur rahman <abdur.rahman200301@gmail.com>
2021-05-12 18:39:06 +0600

    fetch data

---
+++ components/EventItem.js
@@ -0,0 +1,31 @@
+import Link from "next/link";
+import Image from "next/image";
+import styles from "@/styles/EventItem.module.css";
+
+export default function EventItem({ evt }) {
+  const { name, image, date, time, slug } = evt;
+  return (
+    <div className={styles.event}>
+      <div className={styles.img}>
+        <Image
+          src={image ? image : "/images/event-default.png"}
+          width="170"
+          height="100"
+        />
+      </div>
+      <div className={styles.info}>
+        <span>
+          {" "}
+          {date} At {time}{" "}
+        </span>
+        <h3>{name}</h3>
+      </div>
+
+      <div className={styles.link}>
+        <Link href={`/events/${slug}`}>
+          <a className="btn"> Details</a>
+        </Link>
+      </div>
+    </div>
+  );
+}
--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,33 @@
+import Link from "next/link";
+import EventItem from "@/components/EventItem";
 import Layout from "@/components/Layout";
+import { API_URL } from "@/config/index";
 
-export default function HomePage() {
+export default function HomePage({ events }) {
   return (
     <Layout>
-      <h1> Hello Home </h1>
+      <h1> Upcoming Events </h1>
+
+      {events.length === 0 && <h3>No events to show</h3>}
+
+      {events.map((evt) => (
+        <EventItem key={evt.id} evt={evt} />
+      ))}
+      {events.length > 0 && (
+        <Link href="/events" >
+          <a className="btn-secondary"> View All Events</a>
+        </Link>
+      )}
     </Layout>
   );
+}
+
+export async function getStaticProps() {
+  const res = await fetch(`${API_URL}/api/events`);
+  const events = await res.json();
+
+  return {
+    props: { events: events.slice(0, 5) },
+    revalidate: 1,
+  };
 }
